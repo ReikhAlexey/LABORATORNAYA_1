f2e54b2eadd41f9aa5047386c1b97abff384b08b
alex_AVTF <alex527student@ASTRAL>
2026-04-02 20:21:30 +0700

    Ghfrnbrf

--- main.cpp
+++ main.cpp
@@ -107,7 +107,7 @@ void calc_circle() {
 }
 
 
-int main() { //Коментарий
+int main() {
     std::cout << "Проект: Геометрические фигуры" << std::endl;
     setlocale(LC_ALL, "ru_RU.UTF-8");
     int choice = 0;
